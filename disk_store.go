e7d415e564f6de670f8cde326502e75ffe4b64e9
Jaeyoon Cho <jaeyoon.c@gmail.com>
2024-11-17 15:00:03 +0900

    Update disk_store.go

--- disk_store.go
+++ disk_store.go
@@ -1,74 +1,88 @@
 package caskdb
 
-import (
-	"errors"
-	"io/fs"
-	"os"
-)
+// format file provides encode/decode functions for serialisation and deserialisation
+// operations
+//
+// format methods are generic and does not have any disk or memory specific code.
+//
+// The disk storage deals with bytes; you cannot just store a string or object without
+// converting it to bytes. The programming languages provide abstractions where you
+// don't have to think about all this when storing things in memory (i.e. RAM).
+// Consider the following example where you are storing stuff in a hash table:
+//
+//    books = {}
+//    books["hamlet"] = "shakespeare"
+//    books["anna karenina"] = "tolstoy"
+//
+// In the above, the language deals with all the complexities:
+//
+//    - allocating space on the RAM so that it can store data of `books`
+//    - whenever you add data to `books`, convert that to bytes and keep it in the memory
+//    - whenever the size of `books` increases, move that to somewhere in the RAM so that
+//      we can add new items
+//
+// Unfortunately, when it comes to disks, we have to do all this by ourselves, write
+// code which can allocate space, convert objects to/from bytes and many other operations.
+//
+// This file has two functions which help us with serialisation of data.
+//
+//    encodeKV - takes the key value pair and encodes them into bytes
+//    decodeKV - takes a bunch of bytes and decodes them into key value pairs
+//
+//**workshop note**
+//
+//For the workshop, the functions will have the following signature:
+//
+//    func encodeKV(timestamp uint32, key string, value string) (int, []byte)
+//    func decodeKV(data []byte) (uint32, string, string)
 
-// DiskStore is a Log-Structured Hash Table as described in the BitCask paper. We
-// keep appending the data to a file, like a log. DiskStorage maintains an in-memory
-// hash table called KeyDir, which keeps the row's location on the disk.
+// headerSize specifies the total header size. Our key value pair, when stored on disk
+// looks like this:
 //
-// The idea is simple yet brilliant:
-//   - Write the record to the disk
-//   - Update the internal hash table to point to that byte offset
-//   - Whenever we get a read request, check the internal hash table for the address,
-//     fetch that and return
+//	┌───────────┬──────────┬────────────┬─────┬───────┐
+//	│ timestamp │ key_size │ value_size │ key │ value │
+//	└───────────┴──────────┴────────────┴─────┴───────┘
 //
-// KeyDir does not store values, only their locations.
+// This is analogous to a typical database's row (or a record). The total length of
+// the row is variable, depending on the contents of the key and value.
 //
-// The above approach solves a lot of problems:
-//   - Writes are insanely fast since you are just appending to the file
-//   - Reads are insanely fast since you do only one disk seek. In B-Tree backed
-//     storage, there could be 2-3 disk seeks
+// The first three fields form the header:
 //
-// However, there are drawbacks too:
-//   - We need to maintain an in-memory hash table KeyDir. A database with a large
-//     number of keys would require more RAM
-//   - Since we need to build the KeyDir at initialisation, it will affect the startup
-//     time too
-//   - Deleted keys need to be purged from the file to reduce the file size
+//	┌───────────────┬──────────────┬────────────────┐
+//	│ timestamp(4B) │ key_size(4B) │ value_size(4B) │
+//	└───────────────┴──────────────┴────────────────┘
 //
-// Read the paper for more details: https://riak.com/assets/bitcask-intro.pdf
-//
-// DiskStore provides two simple operations to get and set key value pairs. Both key
-// and value need to be of string type, and all the data is persisted to disk.
-// During startup, DiskStorage loads all the existing KV pair metadata, and it will
-// throw an error if the file is invalid or corrupt.
-//
-// Note that if the database file is large, the initialisation will take time
-// accordingly. The initialisation is also a blocking operation; till it is completed,
-// we cannot use the database.
-//
-// Typical usage example:
-//
-//		store, _ := NewDiskStore("books.db")
-//	   	store.Set("othello", "shakespeare")
-//	   	author := store.Get("othello")
-type DiskStore struct {
+// These three fields store unsigned integers of size 4 bytes, giving our header a
+// fixed length of 12 bytes. Timestamp field stores the time the record we
+// inserted in unix epoch seconds. Key size and value size fields store the length of
+// bytes occupied by the key and value. The maximum integer
+// stored by 4 bytes is 4,294,967,295 (2 ** 32 - 1), roughly ~4.2GB. So, the size of
+// each key or value cannot exceed this. Theoretically, a single row can be as large
+// as ~8.4GB.
+const headerSize = 12
+
+// KeyEntry keeps the metadata about the KV, specially the position of
+// the byte offset in the file. Whenever we insert/update a key, we create a new
+// KeyEntry object and insert that into keyDir.
+type KeyEntry struct {
 }
 
-func isFileExists(fileName string) bool {
-	// https://stackoverflow.com/a/12518877
-	if _, err := os.Stat(fileName); err == nil || errors.Is(err, fs.ErrExist) {
-		return true
-	}
-	return false
-}
-
-func NewDiskStore(fileName string) (*DiskStore, error) {
+func NewKeyEntry(timestamp uint32, position uint32, totalSize uint32) KeyEntry {
 	panic("implement me")
 }
 
-func (d *DiskStore) Get(key string) string {
+func encodeHeader(timestamp uint32, keySize uint32, valueSize uint32) []byte {
 	panic("implement me")
 }
 
-func (d *DiskStore) Set(key string, value string) {
+func decodeHeader(header []byte) (uint32, uint32, uint32) {
 	panic("implement me")
 }
 
-func (d *DiskStore) Close() bool {
+func encodeKV(timestamp uint32, key string, value string) (int, []byte) {
+	panic("implement me")
+}
+
+func decodeKV(data []byte) (uint32, string, string) {
 	panic("implement me")
 }
